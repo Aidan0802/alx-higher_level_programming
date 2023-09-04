94f9799be816e472643e3e4c64eb9aa5748757b9
aidan <aidanheydenrycht@gmail.com>
2023-09-04 15:35:34 +0300

    [Created] C file function

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - Checks single linked list for cycles
+ * @list: Link list
+ * @Return: 0 (False), 1 (True)
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *check = list;
+	listint_t *match = list->next;
+
+
+	while (check != match)
+	{
+		if (!match || !match->next)
+			return (0);
+		check = check->next;
+		match = match->next->next;
+	}
+	return (1);
+}
