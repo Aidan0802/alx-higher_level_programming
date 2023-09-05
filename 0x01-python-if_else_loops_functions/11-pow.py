598cec86a44245ff33faa1ecf3d5ba0c8793580a
aidan <aidanheydenrycht@gmail.com>
2023-09-05 22:44:13 +0300

    [Fix] output

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def pow(a, b):
+    if a == 0:
+        return 0
+    if b == 0:
+        return 1
+
+    if b < 0:
+        a = 1 / a
+        b = -b
+
     res = 1
     for _ in range(b):
         res *= a
