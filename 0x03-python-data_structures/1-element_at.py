0ed848d769f0b8e515bd44a196eaafc167ebfb3c
aidan <aidanheydenrycht@gmail.com>
2023-09-09 12:15:38 +0300

    [Fix] range

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     if idx < 0:
         return None
     for i in my_list:
-        if idx not in range(my_list[i]):
+        if idx not in range(len(my_list)):
             return None
         elif:
             return my_list(idx)
