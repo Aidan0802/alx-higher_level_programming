67a565e3875d596c7732bb1ba12adf6823424eea
aidan <aidanheydenrycht@gmail.com>
2023-09-09 18:22:26 +0300

    [Fix] configured range

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    rev = my_list[::-1]
-    for i in rev:
+    for i in range(len(my_list), 0, -1):
         print("{:d}".format(i))
+
+
+
+my_list = [1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
