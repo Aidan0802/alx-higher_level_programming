cbbc4aa69169909885c279bc962bee91a3d58ac9
aidan <aidanheydenrycht@gmail.com>
2023-09-09 17:31:33 +0300

    [Created] python script

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) >= 2:
+        a1, a2 = tuple_a[:2]
+        if a1 < 0:
+            a1 = 0
+        if a2 < 0:
+            a2 = 0
+    else:
+        a1 = tuple_a[0] if len(tuple_a) >= 1 else 0
+        a2 = tuple_a[1] if len(tuple_a) >= 2 else 0
+    if len(tuple_b) >= 2:
+        b1, b2 = tuple_b[:2]
+        if b1 < 0:
+            b1 = 0
+        if b2 < 0:
+            b2 = 0
+    else:
+        b1 = tuple_b[0] if len(tuple_b) >= 1 else 0
+        b2 = tuple_b[1] if len(tuple_b) >= 2 else 0
+        
+    first = a1 + b1
+    second = a2 + b2
+
+    return (first, second)
