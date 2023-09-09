fc6eccd7a4985d33d6eba6b3a25e1cc150aa2984
aidan <aidanheydenrycht@gmail.com>
2023-09-09 13:01:47 +0300

    [Fix] requirement fail

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element): 
-    list_cpy = my_list[:]
+    list_cpy = list(my_list)
     if idx < 0 or idx not in range(len(my_list)):
             return my_list
     list_cpy[idx] = element
