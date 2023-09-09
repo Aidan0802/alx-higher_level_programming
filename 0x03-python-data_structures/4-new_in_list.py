51c6f3f363cbee1069757d62bf73723c398d766c
aidan <aidanheydenrycht@gmail.com>
2023-09-09 13:00:31 +0300

    [Fix] removed text

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,13 +5,3 @@ def new_in_list(my_list, idx, element):
             return my_list
     list_cpy[idx] = element
     return list_cpy
-
-
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-
-print(new_list)
-print(my_list)
