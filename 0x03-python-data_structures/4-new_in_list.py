126fd425b5a7a3c9db88cc6932c1275d847bbeb9
aidan <aidanheydenrycht@gmail.com>
2023-09-09 13:03:04 +0300

    [Fix] pycodestyle

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element): 
-    list_cpy = list(my_list)
+def new_in_list(my_list, idx, element):
+    list_cpy = my_list[:]
     if idx < 0 or idx not in range(len(my_list)):
-            return my_list
+        return my_list
     list_cpy[idx] = element
     return list_cpy
