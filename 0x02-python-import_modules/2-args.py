2ef716d2f1bfadb319958687cfdee9feb756c69b
aidan <aidanheydenrycht@gmail.com>
2023-09-13 12:12:52 +0300

    [Fix] requirement fail

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-argv = sys.argv[1:]
-argc = 0
-length = len(argv)
-if length > 0:
-    print("{} argumets:".format(length))
-    while argc < length:
-        arg = argv[argc]
-        print("{}: {}".format(argc + 1, arg))
-        argc += 1
-else:
-    print("{} arguments:".format(length))
+    argv = sys.argv[1:]
+    argc = 0
+    length = len(argv)
+    if length > 0:
+        print("{} argumets:".format(length))
+        while argc < length:
+            arg = argv[argc]
+            print("{}: {}".format(argc + 1, arg))
+            argc += 1
+    else:
+        print("{} arguments.".format(length))
