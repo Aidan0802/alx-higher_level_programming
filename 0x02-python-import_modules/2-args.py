74c0f4dc0f12e2ad7d47de6220ddbc6a990db3f3
aidan <aidanheydenrycht@gmail.com>
2023-09-13 12:10:37 +0300

    [Fix] output

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,12 @@ import sys
 
 argv = sys.argv[1:]
 argc = 0
-while argc < len(argv):
-    arg = argv[argc]
-    print("{}: {}".format(argc + 1, arg))
-    argc += 1
+length = len(argv)
+if length > 0:
+    print("{} argumets:".format(length))
+    while argc < length:
+        arg = argv[argc]
+        print("{}: {}".format(argc + 1, arg))
+        argc += 1
+else:
+    print("{} arguments:".format(length))
