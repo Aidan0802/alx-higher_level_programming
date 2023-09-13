aa2c00ee2275595c4f60a7c48f795954c715ab74
aidan <aidanheydenrycht@gmail.com>
2023-09-13 17:31:43 +0300

    [Fix] remove test code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,16 +6,3 @@ def square_matrix_simple(matrix=[]):
         for col in range(len(matrix[row])):
             new_matrix[row][col] *= matrix[row][col]
     return new_matrix
-
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
-
