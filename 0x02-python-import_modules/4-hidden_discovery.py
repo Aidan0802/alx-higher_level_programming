0be611ace56204365389b9e62eb603a875533c43
aidan <aidanheydenrycht@gmail.com>
2023-09-13 13:35:06 +0300

    [Fix] output

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import dis
+import dis
 
+if __name__ == "__main__":
     with open('hidden_4.pyc:Zone.Identifier', 'rb') as file:
         code = file.read()
         dis.dis(code)
+        names = dir(code)
 
-    if i in code:
-        if i[0] != '_' and i[1] != '_':
-            print("{}".format(i))
+    for name in sorted(names):
+        if not name.startswith('__'):
+            print(name)
