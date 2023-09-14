e4dd28c4b42a9615b8aa789cbd05192c5892ed19
aidan <aidanheydenrycht@gmail.com>
2023-09-14 11:27:03 +0300

    [Fix] return function

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-
-    for key in a_dictionary:
-        print("{}: {}".format(key, a_dictionary[key]))
-
+    return a_dictionary
