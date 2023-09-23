9c8daa985492bfc4b15fc937b4a3cab407ae726e
aidan <aidanheydenrycht@gmail.com>
2023-09-23 16:25:27 +0300

    [Fix] removed semicolon

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in my_list:
             if i > x:
-                break;
+                break
             print(i, end="")
             count += 1
     except e:
