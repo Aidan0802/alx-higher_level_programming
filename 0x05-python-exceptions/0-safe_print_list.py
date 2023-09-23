be34f3b3bc760c498e734324f9152dcf86ff28e3
aidan <aidanheydenrycht@gmail.com>
2023-09-23 16:23:24 +0300

    [Created] Python script

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for i in my_list:
+            if i > x:
+                break;
+            print(i, end="")
+            count += 1
+    except e:
+        print("Error: Element is not printable.")
+    finally:
+        print()
+    return count
