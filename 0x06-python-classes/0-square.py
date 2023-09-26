79ce4e800c52343f5e023cc03d8d90ea806457fd
aidan <aidanheydenrycht@gmail.com>
2023-09-26 20:45:16 +0300

    [Fix] Documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 class Square:
     """
+
     This Class does nothing
     Attributes:
         None
+
     """
     def __init__(self):
         """
         Initializes a new instance of the Square class.
         """
+        
         pass
