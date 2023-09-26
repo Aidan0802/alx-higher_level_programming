def0e9c3f93c73a5a9683e51a4528ccb73ca3542
aidan <aidanheydenrycht@gmail.com>
2023-09-26 23:05:15 +0300

    [Fix] doc

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 class Square:
     """
+
     Empty class Square that defines a square.
 
     Attributes:
         None
+
     """
     pass
-
-my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
