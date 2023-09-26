c0a31c08e2dffac1bd8290b37f76b83e6a7f90fd
aidan <aidanheydenrycht@gmail.com>
2023-09-27 00:19:38 +0300

    [Created] Python Class

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Define class sqaure
+"""
+
+
+class Square:
+    """Class Square that defines a square.
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of Square class.
+        Args: param1: The size of the Square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter method for retrieving the size.
+        Returns: int: Size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method for setting the size.
+        Args:
+            value: Size to set.
+        Raises:
+            TypeError: If value is not integer
+            ValueError: if value is not >= to 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates and returns area of square
+        Return: int: Area of square
+        """
+        return self.__size * self.__size
+    
+    def my_print(self):
+        if self.__size > 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
