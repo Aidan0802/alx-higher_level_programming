872b192e29476e69c58ea40be839c8f0e6202ea9
aidan <aidanheydenrycht@gmail.com>
2023-09-26 21:51:51 +0300

    [Created] Python Class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square:
+    """Class Square that defines a square
+    """
+    def __init__(self, size):
+        """
+        Initializes a new instance of Square class
+
+        Args: param1: The size of the Square.
+        """
+        self.__size = size
