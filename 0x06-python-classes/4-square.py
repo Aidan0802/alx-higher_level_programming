d5d850008690528eba469c67fec4ed8f91116784
aidan <aidanheydenrycht@gmail.com>
2023-09-26 22:54:49 +0300

    [Fix] ouput

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -8,7 +8,7 @@ class Square:
 
         Args: param1: The size of the Square.
         """
-        self.size = size
+        self.__size = size
 
     def size(self):
         """Getter method for retrieving the size.
@@ -16,7 +16,7 @@ class Square:
         """
         return self.__size
 
-    def size(self, value):
+    def size(self, size):
         """Setter method for setting the size.
         Args:
             value: Size to set.
@@ -24,7 +24,7 @@ class Square:
             TypeError: If value is not integer
             ValueError: if value is not >= to 0
         """
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
@@ -36,4 +36,23 @@ class Square:
  
         Return: int: Area of square
         """
-        return self.size * self.size
+        return self.__size * self.__size
+
+
+
+
+
+
+
+
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
