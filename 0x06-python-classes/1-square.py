06107ad80272f4cbf091323a541cb117c9268dfb
aidan <aidanheydenrycht@gmail.com>
2023-09-26 21:52:40 +0300

    [Fix] parameter

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,7 +2,7 @@
 class Square:
     """Class Square that defines a square
     """
-    def __init__(self, size=0):
+    def __init__(self, size):
         """
         Initializes a new instance of Square class
 
