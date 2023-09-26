2e3b662a130882bcd0a816df1bf97ad0b75e9e34
aidan <aidanheydenrycht@gmail.com>
2023-09-26 22:57:43 +0300

    [Fix] remove test code

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,7 +5,6 @@ class Square:
     def __init__(self, size=0):
         """
         Initializes a new instance of Square class.
-
         Args: param1: The size of the Square.
         """
         self.__size = size
@@ -33,26 +32,6 @@ class Square:
 
     def area(self):
         """Calculates and returns area of square
- 
         Return: int: Area of square
         """
         return self.__size * self.__size
-
-
-
-
-
-
-
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
