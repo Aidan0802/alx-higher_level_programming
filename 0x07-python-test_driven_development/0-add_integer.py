4d1dd9fe44bc8b1ca397150ec988fd194054390f
aidan <aidanheydenrycht@gmail.com>
2023-10-01 19:46:48 +0200

    [Modified] unittest for 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -17,5 +17,11 @@ def add_integer(a, b=98):
         TypeError: If parm1 or parm2 are not integer/float
 
     """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
 
+    a = int(a)
+    b = int(b)
     return a + b
