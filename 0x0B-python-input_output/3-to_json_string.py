b445de30b1a2b13c5307af7bbf285a915f89a8e7
aidan <aidanheydenrycht@gmail.com>
2023-10-10 12:16:41 +0200

    [Fix] whitespaces

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -7,7 +7,7 @@ import json
 
 
 def to_json_string(my_obj):
-    """This function that returns the JSON representation 
+    """This function that returns the JSON representation
     of an object (string)
 
     Args:
