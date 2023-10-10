7c8cec3a8f842a19e16f54db83f13932993d99c4
aidan <aidanheydenrycht@gmail.com>
2023-10-10 10:22:24 +0200

    [Created] Python function

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+This module contains a function that returns an 
+object (Python data structure) represented by a JSON string
+
+"""
+import json
+
+
+def from_json_string(my_str):
+    parsed_data = json.loads(my_str)
+
+    return parsed_data
