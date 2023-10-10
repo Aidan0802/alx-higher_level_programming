a41f9ad0098f2414c30ce5cfa0556cd8bfcb39a7
aidan <aidanheydenrycht@gmail.com>
2023-10-10 19:37:45 +0200

    [Created] Python Class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+
+This module contains a class Student
+
+"""
+
+
+class Student:
+    """This is a class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new instance of Student
+
+        Args:
+            parm1: Student first name
+            parm2: Student last name
+            parm3: Student age
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance
+        Raises:
+            TypeError: First/Last name must be a string
+            TypeError: Age must be a integer
+            ValueError: Age should be equal or greater than 0
+        """
+        student_dict = {
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "age": self.age
+                }
+        return student_dict
