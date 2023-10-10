a919c8a7d6cacc89b67ca0f0b9b32adcbcecdbc4
aidan <aidanheydenrycht@gmail.com>
2023-10-10 09:15:56 +0200

    [Created] Python function

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+This module contains a function that read text file
+
+"""
+
+
+def read_file(filename=""):
+    """Reads a file input
+    Args:
+        parm1: Textfile input
+    """
+    with open(filename, 'r', encoding="utf-8") as file:
+        read_data = file.read()
+
