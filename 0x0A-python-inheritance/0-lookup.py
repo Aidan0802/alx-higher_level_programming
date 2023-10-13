516d6b48404a81e9506f8acfc5ef73941e0a524c
aidan <aidanheydenrycht@gmail.com>
2023-10-13 12:28:59 +0200

    [Fix] removed whitespaces

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
 
-This module contains a function that 
+This module contains a function that
 returns the list of available attributes and methods of an object
 
 """
 
 
 def lookup(obj):
-    """Looks up and returns the the list of available attributes 
+    """Looks up and returns the the list of available attributes
     and methods of an object
 
     Args:
