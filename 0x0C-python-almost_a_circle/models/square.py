6fb63349b62a63cffb2324dbfb4cb1cb110212f1
aidan <aidanheydenrycht@gmail.com>
2023-10-19 16:24:41 +0200

    [Created] class Sqaure

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module contains a class Square that inherits
+from the class Rectangle
+
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """This class creates a square"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Override str method"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
