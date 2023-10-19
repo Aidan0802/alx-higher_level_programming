df467077b6b11e7204d50e571d33c4d30e082c95
aidan <aidanheydenrycht@gmail.com>
2023-10-19 09:55:05 +0200

    [Created] base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+
+This module contains a class Base for managing id 
+attributes in future classes
+
+"""
+
+
+class Base:
+    """Base class for managing id attributes in future classes"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initialize an instance of class base
+        Args:
+            parm1: integer
+        """
+        if isinstance(id, int):
+            self.id = id
+        else:
+          Base.__nb_objects += 1
+          self.id = Base.__nb_objects
