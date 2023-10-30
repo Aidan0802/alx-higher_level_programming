5a67eef28a5f35b562ef22fea0e2a8e382f46c00
aidan <aidanheydenrycht@gmail.com>
2023-10-30 23:41:12 +0200

    [Fix] spelling error

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -11,4 +11,4 @@ def add_attribute(obj, attr_name, attr_val):
 
     if not hasattr(obj, '__dict__'):
         raise TypeError("can't add new attribute")
-    setattr(obj 'attr_name', attr_value)
+    setattr(obj, attr_name, attr_val)
