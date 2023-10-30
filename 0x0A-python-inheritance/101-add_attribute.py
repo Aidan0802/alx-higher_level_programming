eb9193bda112bc20e1ca3f1fe92cb0d4691bb4c5
aidan <aidanheydenrycht@gmail.com>
2023-10-30 23:29:33 +0200

    [Modified] TypeError message

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -10,4 +10,4 @@ def add_attribute(obj, attr_name, attr_val):
     setattr(obj, attr_name, attr_val)
 
     if not hasattr(obj, attr_name) or getattr(obj, attr_name) != attr_val:
-        raise TypeError("can't add new attribute if the object can't have new attribute")
+        raise TypeError("can't add new attribute")
