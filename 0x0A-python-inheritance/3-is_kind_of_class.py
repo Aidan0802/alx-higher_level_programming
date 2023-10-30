e59c984696329dcc8e45e3bfccfe95a1d456f278
aidan <aidanheydenrycht@gmail.com>
2023-10-30 21:22:19 +0200

    [Created] Python Script

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+This module contains a function is_kind_of_class
+
+"""
+
+
+def is_kind_od_class(obj, a_class):
+    """If object is the exact instance of or inherit
+    of specified class return true
+    """
+    return isinstance(obj, a_class)
