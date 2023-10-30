c22176e713fbddc40cd147b262e79aa70da2167c
aidan <aidanheydenrycht@gmail.com>
2023-10-30 23:01:35 +0200

    [Fix] removed super() arguments

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,11 +9,12 @@ Rectangle = __import__('9-rectangle').BaseGeometry
 
 class Square(Rectangle):
     """This class inherits from class Rectangle"""
+
     def __init__(self, size):
         """Initialize an instance"""
+        super().__init__()
         self.__size = size
         self.integer_validator("size", size)
-        super().__init__(size, size)
 
     def area(self):
         """Method that return the area"""
