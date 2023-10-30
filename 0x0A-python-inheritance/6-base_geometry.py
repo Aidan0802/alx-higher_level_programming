4ab5863c4af70ec7d9d1125df9f383c08420b199
aidan <aidanheydenrycht@gmail.com>
2023-10-30 21:51:24 +0200

    [Created] Python Script

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#/usr/bin/python3
+"""
+
+This module contains an class BaseGeometry
+
+"""
+
+
+class BaseGeometry:
+    """This is a class BaseGeometry
+    Raises:
+        Exception: area() is not implemented"
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
