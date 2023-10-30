c78983843e7bc5536f2ed9039f83717ef8c5a7a9
aidan <aidanheydenrycht@gmail.com>
2023-10-30 23:10:17 +0200

    [Created] Python Script

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+
+This module contains an class Square
+
+"""
+Rectangle = __import__('9-rectangle').BaseGeometry
+
+
+class Square(Rectangle):
+    """This class inherits from class Rectangle"""
+
+    def __init__(self, size):
+        """Initialize an instance"""
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__()
+
+    def area(self):
+        """Method that return the area"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Method that return string"""
+        return f"[Square] {self.__size}/{self.__size}"
